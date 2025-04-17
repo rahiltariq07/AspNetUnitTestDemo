f7c237a189444f74ad879bc220e425851a7fdf16
rahiltariq07 <rahiltariq07@gmail.com>
2025-04-17 16:39:22 +0530

    XUnit Testing

---
+++ xunitExample/xunitExample.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using xunitExample.Controllers;
+
+namespace xunitExample.Tests.Controllers
+{
+    public class HomeControllerTest
+    {
+        //[Fact]
+        //public void HomeController_Index_ValidResult()
+        //{
+        //    // AAA Methodolgy
+        //    // Arrange
+        //    HomeController controller = new HomeController();
+        //    string expectedResult = "Hello, World!";
+
+        //    // Act
+        //    string result = controller.Index();
+
+        //    // Assert
+        //    Assert.Equal(expectedResult, result);
+        //}
+
+        [Fact]
+        public void HomeController_Index_NegativeNumber()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+            int number = -5;
+            string expectedResult = "Negative number";
+            // Act
+            string result = controller.Index(number);
+            // Assert
+            Assert.Equal(expectedResult, result);
+            //Assert.IsType<string>(result);
+        }
+
+        [Fact]
+        public void HomeController_Index_Zero()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+            int number = 0;
+            string expectedResult = "Zero";
+            // Act
+            string result = controller.Index(number);
+            // Assert
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Fact]
+        public void HomeController_Index_PositiveNumber()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+            int number = 5;
+            string expectedResult = "Positive number";
+            // Act
+            string result = controller.Index(number);
+            // Assert
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
---
+++ xunitExample/xunitExample/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace xunitExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        //    public string Index()
+        //    {
+        //        return "Hello, World!";
+        //    }
+        //}
+        public string Index(int number)
+        {
+            if (number < 0)
+            {
+                return "Negative number";
+            }
+            else if (number == 0)
+            {
+                return "Zero";
+            }
+            else
+            {
+                return "Positive number";
+            }
+        }
+    }
+}
